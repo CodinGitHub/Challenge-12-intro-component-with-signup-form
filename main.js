9921e59766f1a0259211caf3055fd86ffac23588
CodinGitHub <gms2programador@gmail.com>
2022-06-04 06:35:11 -0500

    script for video

--- main.js
+++ main.js
@@ -3,40 +3,45 @@ const lastName = document.querySelector('#lastNameInput');
 const emailAddress = document.querySelector('#emailInput');
 const password = document.querySelector('#passInput');
 
+const firstNameError = document.querySelector('#firstNameError');
+const lastNameError = document.querySelector('#lastNameError');
+const emailAddressError = document.querySelector('#emailAddressError');
+const passError = document.querySelector('#passError');
+
 const button = document.querySelector('#button');
 
 button.addEventListener('click', (event)=>{
     event.preventDefault();
     validateEmpty(firstName.value, firstName, firstNameError, 'First Name');
     validateEmpty(lastName.value, lastName, lastNameError, 'Last Name');
-    validateEmail(emailAddress.value, emailAddress);
+    validateEmail(emailAddress.value, emailAddress, emailAddressError)
     validateEmpty(password.value, password, passError, 'Password');
 });
 
-function validateEmpty(valueInput, divInput, divError, nameInput){
-    if (valueInput.length == 0){    
-        showError(`${nameInput} cannot be empty`, divError, divInput)
-    }else{
-        hideError(divError, divInput);
-    }
-}
-
-function validateEmail(email, divInput){
+function validateEmail(valueInput, divInput, divError){
     let regExp = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
-    if(regExp.test(email)){
-        hideError(emailAddressError, divInput);
+    if(regExp.test(valueInput) == true){
+        hideError(divInput, divError);
     }else{
-        showError('Looks like this is not an email', emailAddressError, divInput)
+        showError(divInput, divError, 'Looks like this is not an email');
     }
 }
 
-function showError(error, divError, divInput){
+function validateEmpty(valueInput, divInput, divError, nameInput){
+    if(valueInput.length == 0){
+        showError(divInput, divError, `${nameInput} cannot be empty`);
+    }else{
+        hideError(divInput, divError);
+    }
+}
+
+function showError(divInput, divError, error){
     divInput.style.border = ' 1px solid red';
     divError.innerHTML = `<img class="icon-error" src="./images/icon-error.svg" alt="">
-    <p class="error">${error}</p>`;
+    <p class="error">${error}</p>`
 }
 
-function hideError(divError, divInput){
-    divError.innerHTML = '';
-    divInput.style.border = ' 1px solid hsl(246, 25%, 77%)'
+function hideError(divInput, divError){
+    divInput.style.border = ' 1px solid hsl(246, 25%, 77%)';
+    divError.innerHTML = ``
 }
